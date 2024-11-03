adb05dc77a1226391eabb4caf6d9ba1f652856bc
Hu Yunhai <alexhu1999@outlook.com>
2024-11-03 10:36:34 -0500

    support parallel

--- batch_client.py
+++ batch_client.py
@@ -49,6 +49,34 @@ class BatchClient:
         # 输出或返回 verified_text 结果
         # print(f"Received verified text for {request_id}: {response.verified_text}")
         return response.verified_text
+    
+
+    def add_async_request(self, request_id, request_type, text):
+        """发送请求到服务器，不阻塞等待返回已验证文本"""
+
+        # 创建请求对象
+        request = batch_pb2.Request(request_id=request_id, request_type=request_type, text=text)
+
+        # 使用异步 future 方法发送请求
+        future = self.stub.AddRequest.future(request)
+
+        # 定义一个回调函数来处理返回结果
+        def handle_response(future):
+            try:
+                # 这里可以捕获请求的返回值
+                response = future.result()  # 获取异步调用结果
+                verified_text = response.verified_text
+                print(f"请求 {request_id} 的验证文本已返回: {verified_text}")
+                # 在此处执行进一步的处理逻辑
+            except Exception as e:
+                # 如果请求失败，可以捕获异常
+                print(f"请求 {request_id} 出现异常: {e}")
+            # 设置回调函数，当 future 完成时调用 handle_response
+
+        future.add_done_callback(handle_response)
+
+        # 返回 future 对象，如果需要进一步处理或等待可以使用
+        return future
 
     def close(self):
         """关闭与服务器的连接"""
@@ -115,13 +143,94 @@ class BatchClient:
             print(f"Verified Successfully.")
             timestamps.append(time.time())
             #print(f"Verified text: {verified_text}")
+        
+    def speculative_decoding_parallel(self, prompt, max_length=20, generate_step=4):
+        """使用 speculative decoding 生成文本"""
+        generate_step = self.args.gamma
+        max_length = self.args.max_tokens
+
+        timestamps = []
+        timestamps.append(time.time())
+        request_id = str(uuid.uuid4().hex)
+        # 模拟发送请求
+        length = 0
+        verified_text_future = self.add_async_request(request_id, "init", prompt)
+        timestamps.append(time.time())
+        input_text = prompt
+        input_ids = self.tokenizer.encode(input_text, return_tensors='pt').to(self.device)
+        old_input_ids = input_ids
+        old_length = len(old_input_ids[0])
+        past_key_values = None
+
+        cur_mode = True
+
+        while length < max_length:
+            new_length = len(input_ids[0])
+            passed_length = new_length - old_length
+            old_input_ids = input_ids 
+            old_length = len(old_input_ids[0])
+            #print(f"Passed length: {passed_length}")
+            length += passed_length
+            draft_outputs = ""
+            while not verified_text_future.done():
+                with torch.no_grad():
+                    Mq = self.model(
+                        input_ids=input_ids,
+                        past_key_values=past_key_values,
+                        use_cache=self.use_cache,
+                    )
+                past_key_values = Mq.past_key_values
+                draft_logits = Mq.logits[..., -1, :]
+                xi = torch.argmax(draft_logits, dim=-1).unsqueeze(-1)
+                if self.use_cache:
+                    input_ids = xi.to(self.device)
+                else:
+                    input_ids = torch.cat((input_ids, xi), dim=1).to(self.device)
+
+                draft_outputs += self.tokenizer.decode(xi[0], skip_special_tokens=True)
+                timestamps.append(time.time()) 
+
+            verified_text = verified_text_future.result().verified_text
+            verified_ids = self.tokenizer.encode(verified_text, return_tensors='pt').to(self.device)
+            draft_ids = self.tokenizer.encode(draft_outputs, return_tensors='pt').to(self.device)
+            if cur_mode:
+                #print(f"verified text: {verified_text}")
+                #print(f"Draft output: {draft_outputs}")
+                if verified_ids[0][-1] == draft_ids[0][0]:
+                    print(f"cur_mode: {not cur_mode}")
+                    cur_mode = False
+                    input_ids = torch.cat((verified_ids, draft_ids[:,1:]), dim=1).to(self.device)
+                    self.add_request(request_id, "update", self.tokenizer.decode(draft_ids[0][1:], skip_special_tokens=True)) 
+                    verified_text_future = self.add_async_request(request_id, "verify", str(len(draft_ids[0])-1))
+                else:
+                    print(f"cur_mode failue: {not cur_mode}")
+                    input_text = verified_text
+                    input_ids = verified_ids
+                    verified_text_future = self.add_async_request(request_id, "init", verified_text)
+            else:
+                #print(f"verified text: {verified_text}")
+                #print(f"Draft output: {draft_outputs}")
+                #print(f"input_text: {input_text}")
+                if torch.equal(verified_ids[0][:old_length], old_input_ids[0]) and verified_ids[0][-1]  == draft_ids[0][0]:
+                    print(f"cur_mode continue: {not cur_mode}")
+                    input_ids = torch.cat((verified_ids, draft_ids[:,1:]), dim=1).to(self.device)
+                    self.add_request(request_id, "update", self.tokenizer.decode(draft_ids[0][1:], skip_special_tokens=True)) 
+                    verified_text_future = self.add_async_request(request_id, "verify", str(len(draft_ids[0])-1))
+                else:
+                    print(f"cur_mode continue failue: {not cur_mode}")
+                    input_ids = verified_ids
+                    cur_mode = True
+                    verified_text_future = self.add_async_request(request_id, "init", verified_text)
             
+            input_text = self.tokenizer.decode(input_ids[0], skip_special_tokens=True)
+            print(f"Next forward input_ids: {self.tokenizer.decode(input_ids[0], skip_special_tokens=True)}")
+            # input_ids = self.tokenizer.encode(input_text, return_tensors='pt').to(self.device)
+            timestamps.append(time.time())
+              
 
         # 等待处理完成
         print(f"final text: {verified_text}")
-
         self.add_request(request_id, "delete", " "+str(length))
-
         return verified_text, timestamps
 
 
@@ -131,5 +240,5 @@ if __name__ == "__main__":
     # 初始化客户端
     args = parse_arguments()
     client = BatchClient(args)
-    ans, timestamps = client.speculative_decoding("The quick brown fox jumps over the lazy dog.")
+    ans, timestamps = client.speculative_decoding_parallel("The quick brown fox jumps over the lazy dog.")
     print(f"timestamps: {timestamps}")
--- batch_server.py
+++ batch_server.py
@@ -15,7 +15,7 @@ request_text = {}
 results = {}
 condition = threading.Condition()
 BATCH_SIZE = 2
-MAX_WAIT_TIME = 2
+MAX_WAIT_TIME = 1
 generate_step = 4
 
 
@@ -25,7 +25,7 @@ class VerficationServer(batch_pb2_grpc.BatchServiceServicer):
         self.args = args
         self.load_model()
         # 启动批处理线程
-        self.batch_thread = threading.Thread(target=self.process_batch, daemon=True)
+        self.batch_thread = threading.Thread(target=self.process_batch_parallel, daemon=True)
         self.batch_thread.start()
 
     def load_model(self):
@@ -49,7 +49,7 @@ class VerficationServer(batch_pb2_grpc.BatchServiceServicer):
             # 处理请求的不同类型
             if request.request_type == "init":
                 if request.request_id not in request_text:
-                    request_queue.append({"request_id": request.request_id, "text": request.text, "request_type": "init"})
+                    request_queue.append({"request_id": request.request_id, "text": request.text, "request_type": "init", "gamma": generate_step})
                     request_text[request.request_id] = request.text
                     print(f"Initialized request {request.request_id}: {request.text}")
                 else:
@@ -70,7 +70,11 @@ class VerficationServer(batch_pb2_grpc.BatchServiceServicer):
                     )
             elif request.request_type == "verify":
                 if request.request_id not in request_queue:
-                    request_queue.append({"request_id": request.request_id, "text": request_text[request.request_id], "request_type": "verify"})
+                    if request.text != "":
+                        request_queue.append({"request_id": request.request_id, "text": request_text[request.request_id], "request_type": "verify", "gamma": int(request.text)})
+                    else:
+                        request_queue.append({"request_id": request.request_id, "text": request_text[request.request_id], "request_type": "verify", "gamma": generate_step})
+
                 print(f"Verification request {request.request_id} added to batch queue.")
             elif request.request_type == "delete":
                 if request.request_id in request_text:
@@ -110,14 +114,15 @@ class VerficationServer(batch_pb2_grpc.BatchServiceServicer):
                     req_id = request["request_id"]
                     job_type = request["request_type"]
                     job_text = request["text"]
-                    batch.append((req_id, job_type, job_text))
+                    gamma = request["gamma"]
+                    batch.append((req_id, job_type, job_text, gamma))
                 else:
                     time.sleep(0.1)  # 无请求时稍作等待
 
             if batch:
                 # 批量处理请求
                 batch_size = len(batch)
-                texts = [req for _, _, req in batch]
+                texts = [req for _, _, req, _ in batch]
                 inputs = self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True)
                 input_ids = inputs.input_ids.to(self.device)
                 attention_mask = inputs.attention_mask.to(self.device)
@@ -173,6 +178,92 @@ class VerficationServer(batch_pb2_grpc.BatchServiceServicer):
                 # 存储批处理结果并通知等待的请求
                 with condition:
                     for idx, (req_id,_, _) in enumerate(batch):
+                        results[req_id] = verified_text[idx]  # 将每个请求的结果存入字典
+                        request_text[req_id] = verified_text[idx]  # 更新请求文本
+                        print(f"Processed result for {req_id}: {results[req_id]}")
+                    condition.notify_all()  # 通知所有等待的请求
+            else:
+                time.sleep(0.1)
+
+        # 批处理逻辑
+    def process_batch_parallel(self):
+        """后台线程，用于批量处理请求"""
+        global request_queue, request_text, results
+        while True:
+            start_time = time.time()  # 每次进入批量处理逻辑时重置 start_time
+            batch = []
+
+            # 从请求队列中收集请求，直到达到批次大小或超时
+            while len(batch) < self.args.batch_size and (time.time() - start_time) < MAX_WAIT_TIME:
+                if request_queue:
+                    request = request_queue.pop(0)
+                    req_id = request["request_id"]
+                    job_type = request["request_type"]
+                    job_text = request["text"]
+                    gamma = request["gamma"]
+                    batch.append((req_id, job_type, job_text, gamma))
+                else:
+                    time.sleep(0.1)  # 无请求时稍作等待
+
+            if batch:
+                # 批量处理请求
+                batch_size = len(batch)
+                texts = [req for _, _, req, _ in batch]
+                inputs = self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True)
+                input_ids = inputs.input_ids.to(self.device)
+                attention_mask = inputs.attention_mask.to(self.device)
+                #print(f"Processing batch: {texts}, {input_ids}")
+
+                with torch.no_grad():
+                    Mp = self.model(
+                            input_ids=input_ids,
+                            attention_mask=attention_mask,
+                            use_cache=False,
+                        ) 
+                target_logits = Mp.logits
+                # 遍历批次中的每个序列
+                verified_text = [None] * batch_size
+                for i in range(batch_size):
+                    job_type = batch[i][1]
+                    if job_type == "init":
+                        print(f"init batch id: {i}, input_text: {texts[i]}")
+                        next_token_logits = target_logits[i, -1, :]
+                        next_token_id = torch.argmax(next_token_logits, dim=-1).unsqueeze(-1)
+                        output = torch.cat((input_ids[i], next_token_id), dim=0)
+
+                        # Convert tensor to text
+                        generated_text = self.tokenizer.decode(output, skip_special_tokens=True)
+                        verified_text[i] = texts[i] + generated_text
+                        print(f"initialized text: {verified_text[i]}")
+                    elif job_type == "verify":
+                        # 对 `draft_logits` 中的第 i 个序列进行 `argmax`
+                        print(f"verify batch id: {i}, input_text: {texts[i]}")
+                        generate_step = int(batch[i][3])
+                        predicted_token_ids = target_logits[i][-generate_step-1:, :].argmax(dim=-1)
+
+                        # 解码预测的 token 和对应输入的 token
+                        #predicted_text = self.tokenizer.decode(predicted_token_ids, skip_special_tokens=True)
+                        #draft_text = self.tokenizer.decode(input_ids[i][-generate_step:], skip_special_tokens=True)
+                        n = generate_step
+                        #print(f"predicted_text: {predicted_text}, draft_text: {draft_text}")
+                        for j in range(generate_step):
+                            if predicted_token_ids[j] == input_ids[i][-generate_step+j]:
+                                continue
+                            else:
+                                n = j
+                                break
+                        if n == generate_step:
+                            verified_text[i] = self.tokenizer.decode(input_ids[i], skip_special_tokens=True) + self.tokenizer.decode(predicted_token_ids[n], skip_special_tokens=True)
+                        else:
+                            verified_text[i] = self.tokenizer.decode(input_ids[i][:-generate_step+n], skip_special_tokens=True) + self.tokenizer.decode(predicted_token_ids[n], skip_special_tokens=True)
+
+                        # 比较预测和输入文本
+                        #print(f"Sample {i+1}")
+                        print("Match:", verified_text[i])
+
+                # 存储批处理结果并通知等待的请求
+                with condition:
+                    for idx, (req_id,_, _, _) in enumerate(batch):
                         results[req_id] = verified_text[idx]  # 将每个请求的结果存入字典
                         request_text[req_id] = verified_text[idx]  # 更新请求文本
                         print(f"Processed result for {req_id}: {results[req_id]}")
